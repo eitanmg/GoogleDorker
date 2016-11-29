bae4f7bfcb3cafe0f34c8120b578d968382ea7b1
Eitan Mugerman <eitanmug@gmail.com>
2016-11-29 16:12:32 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.QueryTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.WLTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LinkToGuide = new System.Windows.Forms.LinkLabel();
+            this.allPagesCB = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // QueryTextBox
+            // 
+            this.QueryTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.QueryTextBox.Location = new System.Drawing.Point(31, 36);
+            this.QueryTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.QueryTextBox.Multiline = true;
+            this.QueryTextBox.Name = "QueryTextBox";
+            this.QueryTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.QueryTextBox.Size = new System.Drawing.Size(760, 229);
+            this.QueryTextBox.TabIndex = 0;
+            this.QueryTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(350, 584);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Dork Google!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(286, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter your query in textbox";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // WLTextBox
+            // 
+            this.WLTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.WLTextBox.Location = new System.Drawing.Point(31, 294);
+            this.WLTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.WLTextBox.Multiline = true;
+            this.WLTextBox.Name = "WLTextBox";
+            this.WLTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.WLTextBox.Size = new System.Drawing.Size(760, 231);
+            this.WLTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(286, 267);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(230, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter Whitelist Domains";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // LinkToGuide
+            // 
+            this.LinkToGuide.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LinkToGuide.AutoSize = true;
+            this.LinkToGuide.Location = new System.Drawing.Point(12, 648);
+            this.LinkToGuide.Name = "LinkToGuide";
+            this.LinkToGuide.Size = new System.Drawing.Size(140, 13);
+            this.LinkToGuide.TabIndex = 5;
+            this.LinkToGuide.TabStop = true;
+            this.LinkToGuide.Text = "How to use this application?";
+            this.LinkToGuide.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkToGuide_LinkClicked);
+            // 
+            // allPagesCB
+            // 
+            this.allPagesCB.AutoSize = true;
+            this.allPagesCB.Location = new System.Drawing.Point(317, 545);
+            this.allPagesCB.Name = "allPagesCB";
+            this.allPagesCB.Size = new System.Drawing.Size(166, 17);
+            this.allPagesCB.TabIndex = 6;
+            this.allPagesCB.Text = "Scan only first page per query";
+            this.allPagesCB.UseVisualStyleBackColor = true;
+            this.allPagesCB.CheckedChanged += new System.EventHandler(this.allPagesCB_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 670);
+            this.Controls.Add(this.allPagesCB);
+            this.Controls.Add(this.LinkToGuide);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.WLTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.QueryTextBox);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Google Dork";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox QueryTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox WLTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel LinkToGuide;
+        private System.Windows.Forms.CheckBox allPagesCB;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string directory = System.IO.Directory.GetCurrentDirectory();
+        string chromeDefaultProfile = System.Configuration.ConfigurationManager.AppSettings["chromeDefaultProfile"];
+        string ExecutablePath = System.Configuration.ConfigurationManager.AppSettings["ExecutablePath"];
+        string chromeDriverPath = System.IO.Directory.GetCurrentDirectory();
+        string googleWebsiteAddress = System.Configuration.ConfigurationManager.AppSettings["googleWebsiteAddress"];
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (QueryTextBox.Text == "")
+            {
+                MessageBox.Show("Please enter something in the Query textbox!", "Error", MessageBoxButtons.OK, MessageBoxIcon.None, MessageBoxDefaultButton.Button1, (MessageBoxOptions)0x40000);
+                return;
+            }
+
+            string[] queryLines = QueryTextBox.Text.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            //Create Chrome default instance
+
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments(chromeDefaultProfile);
+            options.BinaryLocation = ExecutablePath;
+            IWebDriver driver = new ChromeDriver(chromeDriverPath, options);
+            Random r = new Random();
+
+            // Create a file to write to.
+            string dateTimeString = string.Format("Query-{0:yyyy-MM-dd_hh-mm-ss-tt}.txt",DateTime.Now);
+            string path = directory + "\\Logs\\" + dateTimeString;
+            if (!File.Exists(path))
+            {               
+                TextWriter tw = new StreamWriter(path, true);
+                tw.Close();
+            }
+
+            for (int k = 0; k < queryLines.Length; k++)
+            {
+                //query part
+                Uri googleURI_1 = new Uri(driver.Url);
+                driver.Navigate().GoToUrl(googleWebsiteAddress);
+                
+                //Check if its a CAPTCHA page
+                while (googleURI_1.Host == "ipv4.google.com")
+                {
+                    MessageBox.Show("Enter the CAPTCHA! and then click OK", "CAPTCHA", MessageBoxButtons.OK, MessageBoxIcon.None, MessageBoxDefaultButton.Button1, (MessageBoxOptions)0x40000);
+                    Uri stillCapchaUrl = new Uri(driver.Url);
+                    googleURI_1 = stillCapchaUrl;
+                    driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+                }
+
+                IWebElement queryTextBox = driver.FindElement(By.Id("lst-ib"));
+                queryTextBox.SendKeys(queryLines[k]);
+                queryTextBox.Submit();
+
+                Uri googleURI_2 = new Uri(driver.Url);
+
+                //Check if its a CAPTCHA page
+                while (googleURI_2.Host == "ipv4.google.com")
+                {
+                    MessageBox.Show("Enter the CAPTCHA! and then click OK", "CAPTCHA", MessageBoxButtons.OK, MessageBoxIcon.None, MessageBoxDefaultButton.Button1, (MessageBoxOptions)0x40000);
+                    Uri stillCapchaUrl = new Uri(driver.Url);
+                    googleURI_2 = stillCapchaUrl;
+                    driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+                }
+
+                driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(1));
+                IList<IWebElement> rawGetTotalPages = driver.FindElements(By.CssSelector("div[id='navcnt']"));
+                IList<IWebElement> pagesElement = rawGetTotalPages[0].FindElements(By.TagName("a"));
+
+                //first page execution
+                IList<IWebElement> rawGoogleUrls = driver.FindElements(By.TagName("h3"));
+                writeUrlsinFile(rawGoogleUrls, path);
+                driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(r.Next(0, 2)));
+
+                if (allPagesCB.Checked == false)
+                {
+                    for (int i = 0; i < pagesElement.Count; i++)
+                    {
+                        IList<IWebElement> rawCuerrentTotalPages = driver.FindElements(By.CssSelector("div[id='navcnt']"));
+                        IList<IWebElement> nextElement = rawCuerrentTotalPages[0].FindElements(By.CssSelector("a[class='pn']"));
+                        for (int j = 0; j < nextElement.Count; j++)
+                        {
+                            if (nextElement[j].Text == "הבא" || nextElement[j].Text == "Next")
+                            {
+                                nextElement[j].Click();
+                            }
+
+                            Uri googleURI_3 = new Uri(driver.Url);
+
+                            //Check if its a CAPTCHA page
+                            while (googleURI_3.Host == "ipv4.google.com")
+                            {
+                                MessageBox.Show("Enter the CAPTCHA! and then click OK", "CAPTCHA", MessageBoxButtons.OK, MessageBoxIcon.None, MessageBoxDefaultButton.Button1, (MessageBoxOptions)0x40000);
+                                Uri stillCapchaUrl = new Uri(driver.Url);
+                                googleURI_3 = stillCapchaUrl;
+                                driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+                            }
+                        }
+                        driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(r.Next(1, 3)));
+                        IList<IWebElement> GoogleUrls = driver.FindElements(By.TagName("h3"));
+                        writeUrlsinFile(GoogleUrls, path);
+                    }
+                }
+            }
+
+            string[] removeDuplicates = File.ReadAllLines(path);
+            File.WriteAllLines(path, removeDuplicates.Distinct().ToArray());
+            driver.Close();
+            MessageBox.Show("All Done", "done", MessageBoxButtons.OK, MessageBoxIcon.None, MessageBoxDefaultButton.Button1, (MessageBoxOptions)0x40000);
+        }
+
+
+        public void writeUrlsinFile(IList<IWebElement> rawGoogleUrls, string resultPath)
+        {
+            for (int i = 0; i < rawGoogleUrls.Count - 1; i++)
+            {
+                try
+                {
+                    Uri uri = new Uri(rawGoogleUrls[i].FindElement(By.TagName("a")).GetAttribute("href"));
+                    GetDomain getdomain = new GetDomain();
+                    string domainStr = getdomain.GetDomainFromUrl(uri);
+                    bool answer = CheckIfUrlIsLegitimate(domainStr);
+                    if (answer == true)
+                    {
+                        //do nothing
+                    }
+                    else
+                    {
+                        File.AppendAllText(resultPath, rawGoogleUrls[i].FindElement(By.TagName("a")).GetAttribute("href") + Environment.NewLine);
+                    }    
+                }
+                catch (Exception ex)
+                {
+                    string errorDateTimeString = string.Format("Error-{0:yyyy-MM-dd}.txt", DateTime.Now);
+                    string path = directory + "\\Logs\\" + errorDateTimeString;
+                    if (!File.Exists(path))
+                    {
+                        TextWriter tw = new StreamWriter(path, true);
+                        tw.Close();
+                    }
+                    File.AppendAllText(path, DateTime.Now.ToString() + ": " + ex.Message + Environment.NewLine);
+                }
+            }
+        }
+
+        public bool CheckIfUrlIsLegitimate(string url)
+        {
+            string[] WhitelistLines = WLTextBox.Text.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+
+            if (WhitelistLines.Contains(url))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Enable ctrl+A functions in textboxes
+
+        protected override bool ProcessCmdKey(ref Message msg, System.Windows.Forms.Keys keyData)
+        {
+            const int WM_KEYDOWN = 0x100;
+            var keyCode = (System.Windows.Forms.Keys)(msg.WParam.ToInt32() &
+                                  Convert.ToInt32(System.Windows.Forms.Keys.KeyCode));
+            if ((msg.Msg == WM_KEYDOWN && keyCode == System.Windows.Forms.Keys.A)
+                && (ModifierKeys == System.Windows.Forms.Keys.Control)
+                && QueryTextBox.Focused)
+            {
+                QueryTextBox.SelectAll();
+                return true;
+            }
+
+            if ((msg.Msg == WM_KEYDOWN && keyCode == System.Windows.Forms.Keys.A)
+                && (ModifierKeys == System.Windows.Forms.Keys.Control)
+                && WLTextBox.Focused)
+            {
+                WLTextBox.SelectAll();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void LinkToGuide_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string directory = System.IO.Directory.GetCurrentDirectory();
+            string guidePath = directory + "\\Guide\\Guide.html";
+            System.Diagnostics.Process.Start(guidePath);
+        }
+
+        private void allPagesCB_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
